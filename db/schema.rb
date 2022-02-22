ff2e7b3c96193be00594e7c23fad3454821c61f3
Hiago <hiago.andrade.0@gmail.com>
2022-02-22 10:56:53 -0300

    messages scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_22_111033) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_22_134926) do
   create_table "inboxes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_22_111033) do
     t.integer "user_id", null: false
     t.index ["name"], name: "index_inboxes_on_name", unique: true
     t.index ["user_id"], name: "index_inboxes_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "inbox_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["inbox_id"], name: "index_messages_on_inbox_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_22_111033) do
   end
 
   add_foreign_key "inboxes", "users"
+  add_foreign_key "messages", "inboxes"
+  add_foreign_key "messages", "users"
 end
