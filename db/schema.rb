9b6c7c2c9fb101a2dd7e9ad1f5a5c9fae69a39ea
Hiago <hiago.andrade.0@gmail.com>
2022-02-22 08:41:37 -0300

    added user id to inboxes, fixed flash messages not appearing on new devise/sessions/new

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_20_185517) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_22_111033) do
   create_table "inboxes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["name"], name: "index_inboxes_on_name", unique: true
+    t.index ["user_id"], name: "index_inboxes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_20_185517) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inboxes", "users"
 end
