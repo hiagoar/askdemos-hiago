04fe78171e69a0a307d62a9a68c546c601c48db6
Hiago <hiago.andrade.0@gmail.com>
2022-02-23 10:14:36 -0300

    changed sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_02_23_112158) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "inboxes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.integer "messages_count", default: 0, null: false
     t.index ["name"], name: "index_inboxes_on_name", unique: true
     t.index ["user_id"], name: "index_inboxes_on_user_id"
@@ -23,8 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_23_112158) do
 
   create_table "messages", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "inbox_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "inbox_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["inbox_id"], name: "index_messages_on_inbox_id"
