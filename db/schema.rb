c603cfdad87cbbb0aa84fd61df09ec9858b620a2
Hiago <hiago.andrade.0@gmail.com>
2022-02-23 08:59:19 -0300

    counter cache messages to inbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_22_134926) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_23_112158) do
   create_table "inboxes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "messages_count", default: 0, null: false
     t.index ["name"], name: "index_inboxes_on_name", unique: true
     t.index ["user_id"], name: "index_inboxes_on_user_id"
   end
