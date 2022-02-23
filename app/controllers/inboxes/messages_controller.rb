9983baf41397b72965da76f97f29c4477106dc5f
Hiago <hiago.andrade.0@gmail.com>
2022-02-23 07:40:37 -0300

    rubocop

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -1,35 +1,37 @@
-class Inboxes::MessagesController < ApplicationController
-  before_action :set_inbox
+module Inboxes
+  class MessagesController < ApplicationController
+    before_action :set_inbox
 
-  # GET /messages/new
-  def new
-    @message = @inbox.messages.new
-  end
+    # GET /messages/new
+    def new
+      @message = @inbox.messages.new
+    end
 
-  # POST /messages or /messages.json
-  def create
-    @message = @inbox.messages.new(message_params)
+    # POST /messages or /messages.json
+    def create
+      @message = @inbox.messages.new(message_params)
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @inbox, notice: "Message was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
+      respond_to do |format|
+        if @message.save
+          format.html { redirect_to @inbox, notice: 'Message was successfully created.' }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # DELETE /messages/1 or /messages/1.json
-  def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
+    # DELETE /messages/1 or /messages/1.json
+    def destroy
+      @message = Message.find(params[:id])
+      @message.destroy
 
-    respond_to do |format|
-      format.html { redirect_to @inbox, notice: "Message was successfully destroyed." }
+      respond_to do |format|
+        format.html { redirect_to @inbox, notice: 'Message was successfully destroyed.' }
+      end
     end
-  end
 
-  private
+    private
+
     def set_inbox
       @inbox = Inbox.find(params[:inbox_id])
     end
@@ -37,4 +39,5 @@ class Inboxes::MessagesController < ApplicationController
     def message_params
       params.require(:message).permit(:body).merge(user: current_user)
     end
+  end
 end
