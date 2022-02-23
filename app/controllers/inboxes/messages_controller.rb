1afb4d4a463890eba3cdf85411e2a4e7971e18f1
Hiago <hiago.andrade.0@gmail.com>
2022-02-23 07:31:29 -0300

    nest messages inside inboxes

---
+++ app/controllers/inboxes/messages_controller.rb
@@ -0,0 +1,40 @@
+class Inboxes::MessagesController < ApplicationController
+  before_action :set_inbox
+
+  # GET /messages/new
+  def new
+    @message = @inbox.messages.new
+  end
+
+  # POST /messages or /messages.json
+  def create
+    @message = @inbox.messages.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to @inbox, notice: "Message was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /messages/1 or /messages/1.json
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @inbox, notice: "Message was successfully destroyed." }
+    end
+  end
+
+  private
+    def set_inbox
+      @inbox = Inbox.find(params[:inbox_id])
+    end
+
+    def message_params
+      params.require(:message).permit(:body).merge(user: current_user)
+    end
+end
