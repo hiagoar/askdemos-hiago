c87c688aab079be6eab99ea606df4430b721315e
Hiago <hiago.andrade.0@gmail.com>
2022-03-04 10:27:36 -0300

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,29 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Message.delete_all
 Inbox.delete_all
+User.delete_all
 
-5.times do
-	faker_name = Faker::Quote.famous_last_words
-	Inbox.create(name: faker_name)
+2.times do
+	faker_email = Faker::Internet.email
+	user = User.create(email: faker_email,
+										 password: Devise.friendly_token[0, 20])
+
+	rand(1..5).times do
+		faker_name = Faker::Quote.famous_last_words
+		inbox = Inbox.create(name: faker_name,
+												 user: user)
+
+		rand(1..5).times do
+			message_body = Faker::Lorem.paragraph
+			Message.create(body: message_body,
+										 inbox: inbox,
+										 user_id: rand(1..2))
+		end
+	end
 end
+
+p "#{User.count} users created"
+p "#{Inbox.count} inboxes created"
+p "#{Message.count} messages created"
