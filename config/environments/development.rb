b053aba18a9b6504c650fb6276549563e89f2a47
Hiago <hiago.andrade.0@gmail.com>
2022-03-24 09:05:25 -0300

    remove confirmable from devise
    ive done this because confirmation email sending requires SSL authentication, and for that id need to hire a service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,20 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = {host: "http://localhost:3000/", protocol: "https"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  # AMAZON SES
+  config.action_mailer.smtp_settings = {
+    address:              Rails.application.credentials.dig(:aws, :ses, :address),
+    port:                 587,
+    user_name:            Rails.application.credentials.dig(:aws, :ses, :id),
+    password:             Rails.application.credentials.dig(:aws, :ses, :secret),
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -37,7 +51,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
